31f994e9fc229ca68ea761c36cd61aa19146751e
William Rudenmalm <me@whn.se>
2018-06-25 23:21:19 +0200

    Switch to websockets and Redis
    Moves to redis-based storage and redis-based event-bus.

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import cloudevents
+import asyncio
 import aiohttp
+import aioredis
+import ujson
 from sanic import Sanic
 from sanic.response import json, text
 from sanic_cors import CORS
@@ -7,38 +10,7 @@ from sanic_cors import CORS
 app = Sanic("cloudevents-bin", load_env="CE_BIN_")
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-class EventDB(object):
-    def __init__(self):
-        self.db = dict()
-
-    def _get_list(self, k):
-        if k in self.db:
-            return self.db[k]
-        else:
-            l = list()
-            self.db[k] = l
-            return l
-
-    def _add_to_list(self, k, v):
-        self._get_list(k).append(v)
-
-    def log_event(self, namespace, event):
-        key = (namespace, event.event_type)
-        self._add_to_list(key, event)
-        self._add_to_list(namespace, event)
-
-    def get_events_for_type(self, namespace, event_type):
-        key = (namespace, event_type)
-        return self._get_list(key)
-
-    def get_events(self, namespace):
-        return self._get_list(namespace)
-
-    def all_keys(self):
-        return self.db.keys()
-
-
-db = EventDB()
+stop_ws = None
 
 INFO_STR = """CloudEvents is works like but JSON-bin but for CloudEvents. To use it simply start sending webhooks to `/ce/<namespace>` where namespace is an arbitrary string. You can then list your events using /api/<namespace>/events.  Its probably best to use a random ID for you namespace to avoid others using your namespace.  We don't offer any security, your data is visible to anyone who knows your namespace."""
 
@@ -52,10 +24,9 @@ async def info(request):
 
 async def ping_approval_url(url):
     try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get('https://api.github.com/events') as resp:
-                if not resp.status > 400:
-                    print("Response not OK pinging Webhook-Request-Callback at URL '{}'".format(url))
+        async with app.http.get(url) as resp:
+            if not resp.status > 400:
+                print("Response not OK pinging Webhook-Request-Callback at URL '{}'".format(url))
     except:
         print("Something went wrong pining Webhook-Request-Callback at URL '{}'".format(url))
 
@@ -63,7 +34,7 @@ async def ping_approval_url(url):
 async def handle_options(request):
     if 'WebHook-Request-Callback' in request.headers:
         url = request.headers["Webhook-Request-Callback"]
-        ping_approval_url(url)
+        app.add_task(ping_approval_url(url))
 
     resp_headers = {
         'WebHook-Allowed-Origin': '*',
@@ -72,11 +43,16 @@ async def handle_options(request):
     return text("Allowed!", status=200, headers=resp_headers)
 
 
-def handle_post(request, namespace):
+async def handle_post(request, namespace):
     if request.json is None:
         return json({"err": "Only JSON bodies are supported"}, status=400)
     ce = cloudevents.parse(request.json)
-    db.log_event(namespace, ce)
+    data = dict(namespace=namespace, event=ce.to_dict())
+    list_key = key_name("events/" + namespace)
+    push_f = app.redis.lpush(list_key, ujson.dumps(ce.to_dict()))
+    pub_f = app.redis.publish(key_name("feed"), ujson.dumps(data))
+    await asyncio.gather(push_f, pub_f)
+    app.add_task(asyncio.ensure_future(app.redis.ltrim(list_key, 0, 200)))
     return json({"msg": "Got webhook!"}, status=202)
 
 
@@ -85,7 +61,7 @@ async def receive_webhook(request, namespace):
     if request.method == "OPTIONS":
         return await handle_options(request)
     elif request.method == "POST":
-        return handle_post(request, namespace)
+        return await handle_post(request, namespace)
     else:
         return json({"err": "method not allowed"}, status=405)
 
@@ -101,9 +77,83 @@ async def about_namespace(request, namespace):
 
 @app.route("/api/<namespace>/events", methods=["GET"])
 async def get_events(request, namespace):
-    events = db.get_events(namespace)
-    objects = (e.to_dict() for e in events)
+    events = await app.redis.lrange(key_name("events/" + namespace), 0, 100)
+    objects = list(ujson.loads(x) for x in events)
     return json(dict(events=objects))
+
+
+@app.websocket('/api/<namespace>/feed')
+async def event_feed(request, ws, namespace):
+    sender_fn = ws.send
+    try:
+        if namespace not in app.subs:
+            app.subs[namespace] = []
+        app.subs[namespace].append(sender_fn)
+        await stop_ws
+    finally:
+        if namespace in app.subs:
+            app.subs[namespace].remove(sender_fn)
+
+
+async def redis_reader(app, ch):
+    while (await ch.wait_message()):
+        msg = await ch.get_json()
+        if "namespace" not in msg:
+            print("BUG: Message missing namespace - {}".format(msg))
+            continue
+
+        if "event" not in msg:
+            print("BUG: Message missing event - {}".format(msg))
+            continue
+
+        ns = msg["namespace"]
+        for fns in app.subs[ns]:
+            await fns(ujson.dumps(msg["event"]))
+
+
+@app.listener('before_server_stop')
+async def notify_server_stopping(app, loop):
+    stop_ws.set_result(True)
+    await asyncio.sleep(1)
+    app.redis_sub.close()
+
+    app.redis.close()
+    await app.redis_sub.wait_closed()
+    await app.redis.wait_closed()
+
+    await app.http.close()
+
+
+def key_name(x):
+    prefix = app.config.REDIS_PREFIX if "REDIS_PREFIX" in app.config else "CLOUDEVENTS-BIN-"
+    full = prefix + 'CLOUDEVENTS-BIN/'
+    return full + x
+
+@app.listener('before_server_start')
+async def setup_something(app, loop):
+    global stop_ws
+    stop_ws = asyncio.Future(loop=loop)
+    app.subs = dict()
+    redis_sub = await aioredis.create_redis(
+        app.config.REDIS_URL,
+        loop=loop,
+        password=app.config.REDIS_PASSWORD if 'REDIS_PASSWORD' in app.config else None
+    )
+    app.redis_sub = redis_sub
+    app.redis = await aioredis.create_redis(
+        app.config.REDIS_URL,
+        loop=loop,
+        password=app.config.REDIS_PASSWORD if 'REDIS_PASSWORD' in app.config else None
+    )
+
+    async def sub(r):
+        subs = await r.subscribe(key_name("feed"))
+        chan = subs[0]
+        app.add_task(asyncio.ensure_future(redis_reader(app, chan)))
+
+    app.add_task(asyncio.ensure_future(sub(redis_sub)))
+
+    app.http = aiohttp.ClientSession(loop=loop)
 
 if __name__ == "__main__":
     port = int(app.config.PORT) if "PORT" in app.config else 8080
